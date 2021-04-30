d440474996b632ec7e83c7ff73b72665e46bb9c3
Ganesh <ganuofficial2795@gmail.com>
2021-04-30 16:48:28 +0530

    stuff automated

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # Import required modules
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import random
+import random,time
 
 PATH = "C:\Program Files (x86)\chromedriver.exe"
 
@@ -9,15 +9,28 @@ driver = webdriver.Chrome(PATH)
 driver.get("https://www.youtube.com/")
 print(driver.title)
 
+channel_list = ['code with harry','apna college','corey schafer','keith galli','programming hero','freecodecamp','dev ed','telusko','python programmer','kalle hallden','tech with tim','apni kaksha','aman dhattarwal','comicverse']
+channel = random.choice(channel_list)
 
+time.sleep(5)
 
+def search_query():
+    search_query = driver.find_element_by_name('search_query')
+    search_query.send_keys(channel)
 
+search_query()
+time.sleep(4)
 
+def search_click():
 
+    search_btn = driver.find_element_by_id("search-icon-legacy")
+    search_btn.click()
 
-
+search_click()
+time.sleep(4)
 
 
 if __name__ == "__main__":
 
+    driver.quit()
     print("Code Completed 🔥")
